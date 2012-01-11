8c798e9981457a5cc84b37ac3c7ce364f74d4602
Peter Wooley <peterwooley@gmail.com>
2012-01-11 19:46:47 +0000

    Removed unneeded port option.

--- web.js
+++ web.js
@@ -4,6 +4,6 @@ var fileServer = new static.Server();
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-       //fileServer.serve(request, response);
+       fileServer.serve(request, response);
     });
-}).listen(process.env.C9_PORT || process.env.PORT || 80);
+}).listen(process.env.PORT || 80);
